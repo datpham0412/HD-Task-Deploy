fc00231bbc44c1299f76aee9897b2065e0069be2
datpham0412 <100574389+datpham0412@users.noreply.github.com>
2023-09-05 15:32:50 +1000

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello PHP</title>
+</head>
+<body>
+    <div class="text-center">
+        <h1>Hello Dat handsome</h1>
+        <p>Learn about <a href="https://docs.php.net">PHP documentation</a>.</p>
+    </div>
+</body>
+</html>
