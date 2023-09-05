f176a7272230646101a4c6dfae20d778d8cecb5c
datpham0412 <100574389+datpham0412@users.noreply.github.com>
2023-09-05 15:44:07 +1000

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,12 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Hello PHP</title>
+    <title>Hello World</title>
 </head>
 <body>
-    <div class="text-center">
-        <h1>Hello Dat handsome</h1>
-        <p>Learn about <a href="https://docs.php.net">PHP documentation</a>.</p>
-    </div>
+    <h1><?php echo "Hello Dat Handsome!"; ?></h1>
 </body>
 </html>
